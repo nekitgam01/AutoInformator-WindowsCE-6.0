fec091fc577a6b6ce5657482a0743bc56ef9b5bf
nekitgam01 <nekitgam@gmail.com>
2023-03-07 06:49:02 +0500

    Базовый код приложения
    Код уже может воспроизводить wav файлы, читать конфигурационные файлы, работает с порядком озвучки.

---
+++ avid/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace avid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbRecord = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbPath = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbRecord);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbPath);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(638, 93);
+            // 
+            // cbRecord
+            // 
+            this.cbRecord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cbRecord.Location = new System.Drawing.Point(0, 63);
+            this.cbRecord.Name = "cbRecord";
+            this.cbRecord.Size = new System.Drawing.Size(638, 23);
+            this.cbRecord.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(638, 20);
+            this.label2.Text = "Текущая запись";
+            // 
+            // cbPath
+            // 
+            this.cbPath.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cbPath.Items.Add("Вперед");
+            this.cbPath.Items.Add("Обратно");
+            this.cbPath.Location = new System.Drawing.Point(0, 20);
+            this.cbPath.Name = "cbPath";
+            this.cbPath.Size = new System.Drawing.Size(638, 23);
+            this.cbPath.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(638, 20);
+            this.label1.Text = "Направление";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 93);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(638, 362);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(112, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(420, 362);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Повторить";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.Location = new System.Drawing.Point(532, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 362);
+            this.button3.TabIndex = 7;
+            this.button3.Text = ">>";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 362);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "<<";
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(638, 455);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Простой голосовой процессор";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cbRecord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ avid/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Reflection;
+using System.IO;
+
+namespace avid
+{
+    public partial class Form1 : Form
+    {
+        String appDir = "";
+
+        List<string> paths;
+        List<string> events;
+        List<string> mn;
+
+        List<string> ob;
+        List<string> post;
+        List<string> random_yes;
+
+        bool next = false;
+        bool complete = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+
+            paths = new List<string>();
+            events = new List<string>();
+            mn = new List<string>();
+
+            ob = new List<string>();
+            post = new List<string>();
+            random_yes = new List<string>();
+
+            cbPath.SelectedIndex = 0;
+
+            ob.Clear();
+            var path = Path.Combine(appDir, @"ob.txt");
+            FileInfo fi = new FileInfo(path);
+            if (fi.Exists)
+            {
+                using (StreamReader sr = fi.OpenText())
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        ob.Add(s);
+                    }
+                }
+            }
+
+            random_yes.Clear();
+            path = Path.Combine(appDir, @"random_yes.txt");
+            fi = new FileInfo(path);
+            if (fi.Exists)
+            {
+                using (StreamReader sr = fi.OpenText())
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        random_yes.Add(s);
+                    }
+                }
+            }
+
+            post.Clear();
+            path = Path.Combine(appDir, @"post.txt");
+            fi = new FileInfo(path);
+            if (fi.Exists)
+            {
+                using (StreamReader sr = fi.OpenText())
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        post.Add(s);
+                    }
+                }
+            }
+
+            
+            
+
+
+            paths.Clear();
+            cbRecord.Items.Clear();
+
+            path = Path.Combine(appDir, @"path.txt");
+            fi = new FileInfo(path);
+            if (fi.Exists)
+            {
+
+                using (StreamReader sr = fi.OpenText())
+                {
+                    bool b = false;
+                    int state = 0;
+                    string s = "";
+                    string res = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        for (int i = 0; i < s.Length; i++)
+                        {
+                            char c = s[i];
+
+                            if (c == ';')
+                            {
+                                
+                                
+
+                                if (state == 0)
+                                {
+                                    cbRecord.Items.Add(res);
+                                }
+                                else if (state == 1)
+                                {
+                                    paths.Add(res);
+                                }
+                                else if (state == 2)
+                                {
+                                    events.Add(res);
+                                    b = true;
+                                    state = -1;
+                                }
+                                res = "";
+                                state++;
+                            }
+                            else
+                            {
+                                res = res + c;
+                            }    
+                        }
+                        if (b)
+                        {
+                            mn.Add(res);
+                            res = "";
+                            b = false;
+                        }
+                    }
+                }
+
+                cbRecord.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (complete)
+            {
+                if (cbPath.SelectedIndex == 0)
+                {
+                    if (cbRecord.SelectedIndex > 0)
+                    {
+                        cbRecord.SelectedIndex = cbRecord.SelectedIndex - 1;
+                    }
+                }
+                else
+                {
+                    if (cbRecord.SelectedIndex < cbRecord.Items.Count - 1)
+                    {
+                        cbRecord.SelectedIndex = cbRecord.SelectedIndex + 1;
+                    }
+                }
+            }
+
+            if (!next)
+            {
+                var fullPath = Path.Combine(appDir, @"waves\" + paths[cbRecord.SelectedIndex]);
+                SoundPlayer player = new SoundPlayer(fullPath);
+                player.PlaySync();
+                complete = true;
+                next = true;
+            }
+            else
+            {
+
+                for (int j = 0; j < ob.Count; j++)
+                {
+                    var fullPath1 = Path.Combine(appDir, @"waves\" + ob[j]);
+                    SoundPlayer player1 = new SoundPlayer(fullPath1);
+                    player1.PlaySync();
+                }
+
+                var fullPath = Path.Combine(appDir, @"waves\" + events[cbRecord.SelectedIndex]);
+                SoundPlayer player = new SoundPlayer(fullPath);
+                player.PlaySync();
+
+                for (int j = 0; j < ob.Count; j++)
+                {
+                    var fullPath1 = Path.Combine(appDir, @"waves\" + post[j]);
+                    SoundPlayer player1 = new SoundPlayer(fullPath1);
+                    player1.PlaySync();
+                }
+
+                if (mn[cbRecord.SelectedIndex] == "yes")
+                {
+                    Random rnd = new Random();
+                    int i = rnd.Next(0, random_yes.Count);
+                    fullPath = Path.Combine(appDir, @"waves\"+random_yes[i]);
+                    player = new SoundPlayer(fullPath);
+                    player.PlaySync();
+                }
+
+                complete = false;
+                next = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fullPath = Path.Combine(appDir, @"waves\" + paths[cbRecord.SelectedIndex]);
+            SoundPlayer player = new SoundPlayer(fullPath);
+            player.PlaySync();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (complete)
+            {
+                if (cbPath.SelectedIndex == 0)
+                {
+                    if (cbRecord.SelectedIndex < cbRecord.Items.Count - 1)
+                    {
+                        cbRecord.SelectedIndex = cbRecord.SelectedIndex + 1;
+                    }
+                }
+                else
+                {
+                    if (cbRecord.SelectedIndex > 0)
+                    {
+                        cbRecord.SelectedIndex = cbRecord.SelectedIndex - 1;
+                    }
+                }
+            }
+            if (!next)
+            {
+                var fullPath = Path.Combine(appDir, @"waves\" + paths[cbRecord.SelectedIndex]);
+                SoundPlayer player = new SoundPlayer(fullPath);
+                player.PlaySync();
+                complete = true;
+                next = true;
+            }
+            else
+            {
+                
+                for (int j = 0; j < ob.Count; j++)
+                {
+                    var fullPath1 = Path.Combine(appDir, @"waves\"+ob[j]);
+                    SoundPlayer player1 = new SoundPlayer(fullPath1);
+                    player1.PlaySync();
+                }
+
+                var fullPath = Path.Combine(appDir, @"waves\" + events[cbRecord.SelectedIndex]);
+                SoundPlayer player = new SoundPlayer(fullPath);
+                player.PlaySync();
+
+                for (int j = 0; j < ob.Count; j++)
+                {
+                    var fullPath1 = Path.Combine(appDir, @"waves\" + post[j]);
+                    SoundPlayer player1 = new SoundPlayer(fullPath1);
+                    player1.PlaySync();
+                }
+
+                if (mn[cbRecord.SelectedIndex] == "yes")
+                {
+                    Random rnd = new Random();
+                    int i = rnd.Next(0, random_yes.Count);
+                    fullPath = Path.Combine(appDir, @"waves\" + random_yes[i]);
+                    player = new SoundPlayer(fullPath);
+                    player.PlaySync();
+                }
+
+                complete = false;
+                next = false;
+            }
+            
+        }
+    }
+}
